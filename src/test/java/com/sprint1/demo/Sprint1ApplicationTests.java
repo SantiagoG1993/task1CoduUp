58a228daffa1660fc8539c20a41949528d246399
SantiagoG1993 <gamarras1993@gmail.com>
2024-09-06 16:39:49 -0300

    fcommit

---
+++ src/test/java/com/sprint1/demo/Sprint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
